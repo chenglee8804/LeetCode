6cd80d1fa5391cc53405b690eade91f449dd5560
Cheng Li <chenglee8804@gmail.com>
2014-05-05 03:14:51 -0700

    second commit

---
+++ src/FourSum.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class FourSum {
+	public ArrayList<ArrayList<Integer>> fourSum(int[] num, int sum) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,14 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class LongestSubstringWithoutRepeatingCharacters {
 	public int lengthOfLongestSubstring(String s) {
 		int length = s.length();
-		
-		return 0;
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		int start = 0;
+		int maxCount = 0;
+		for(int i = 0; i < length; ++i){
+			if(map.containsKey(s.charAt(i)) && map.get(s.charAt(i)) >= start){
+				maxCount = Math.max(maxCount, i-start);
+				start = map.get(s.charAt(i)) + 1;
+			}
+			map.put(s.charAt(i), i);
+		}
+		maxCount = Math.max(maxCount, s.length() - start);
+		return maxCount;
 	}
 	
 	public static void main(String[] args) {
-		String s = new String("abcabcbb");
+		String s = new String("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco");
 		LongestSubstringWithoutRepeatingCharacters ls = new LongestSubstringWithoutRepeatingCharacters();
-		ls.lengthOfLongestSubstring(s);
+		System.out.println(ls.lengthOfLongestSubstring(s));
 	}
 }
